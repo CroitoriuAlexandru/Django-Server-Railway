13e0daa0b3d4b2e621f72dedd986ab939bf30726
alex <croitoriu.alexandru.code@gmail.com>
2023-10-21 06:48:15 -0900

    save

--- mysite/settings/local.py
+++ mysite/settings/local.py
@@ -1,6 +1,11 @@
 from .base import *
 
 
+STATICFILES_DIRS += [
+    os.path.join(BASE_DIR, "theme/static"),
+    os.path.join(BASE_DIR, "theme/static_src/node_modules/flowbite/dist"),
+    ]
+
 ALLOWED_HOSTS = ["127.0.0.1"]
 
 INTERNAL_IPS = [
