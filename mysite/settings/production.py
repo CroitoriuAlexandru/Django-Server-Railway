368fcc537ee86d5fb2cbfc52147b0d3af3ce9801
alex <croitoriu.alexandru.code@gmail.com>
2023-10-21 05:29:40 -0900

    90% sure :D

--- mysite/settings/production.py
+++ mysite/settings/production.py
@@ -1,7 +1,7 @@
 from .base import *
 
 
-ALLOWED_HOSTS = ["django-server-production-00ab.up.railway.app/"]
+ALLOWED_HOSTS = ["django-server-production-00ab.up.railway.app"]
 
 # FORM SUBMISSION
 # Comment out the following line and place your railway URL, and your production URL in the array.
@@ -9,8 +9,8 @@ CSRF_TRUSTED_ORIGINS = ["https://django-server-production-00ab.up.railway.app"]
 
 SESSION_COOKIE_SECURE = True # handle security.W012
 CSRF_COOKIE_SECURE = True # handle security.W016
-# SECURE_SSL_REDIRECT = True # handle security.W008
-# SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https') # necesary for SECURE_SSL_REDIRECT as it runs with a proxy
+SECURE_SSL_REDIRECT = True # handle security.W008
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https') # necesary for SECURE_SSL_REDIRECT as it runs with a proxy
 
 
 SECURE_HSTS_SECONDS = 31536000 # handle security.W004 
