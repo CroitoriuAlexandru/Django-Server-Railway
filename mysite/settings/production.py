1db0463ef90799bec12809b1f61eb6508151581e
alex <croitoriu.alexandru.code@gmail.com>
2023-10-21 05:19:08 -0900

    added ALLOWED_HOSTS in prod too prev not working

--- mysite/settings/production.py
+++ mysite/settings/production.py
@@ -1,6 +1,9 @@
 from .base import *
 
-ALLOWED_HOSTS = ["django-server-production-00ab.up.railway.app/"]
+ROOT_URLCONF = 'mysite.urls'
+
+
+ALLOWED_HOSTS = ["django-server-production-00ab.up.railway.app/", "127.0.0.1"]
 
 # FORM SUBMISSION
 # Comment out the following line and place your railway URL, and your production URL in the array.
@@ -8,11 +11,14 @@ CSRF_TRUSTED_ORIGINS = ["https://django-server-production-00ab.up.railway.app"]
 
 SESSION_COOKIE_SECURE = True # handle security.W012
 CSRF_COOKIE_SECURE = True # handle security.W016
-SECURE_SSL_REDIRECT = True # handle security.W008
-SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https') # necesary for SECURE_SSL_REDIRECT as it runs with a proxy
+# SECURE_SSL_REDIRECT = True # handle security.W008
+# SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https') # necesary for SECURE_SSL_REDIRECT as it runs with a proxy
 
 
 SECURE_HSTS_SECONDS = 31536000 # handle security.W004 
 SECURE_HSTS_PRELOAD = True # handle security.W019
 SECURE_HSTS_INCLUDE_SUBDOMAINS = True # handle security.W005
  
+print("stdt production settings")
+print( ALLOWED_HOSTS )
+print("end production settings")
