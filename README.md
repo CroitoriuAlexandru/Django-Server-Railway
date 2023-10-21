653372e61afa355219e509243def98cd7a948d71
alex <croitoriu.alexandru.code@gmail.com>
2023-10-21 09:08:24 -0900

    ssasda

--- README.md
+++ README.md
@@ -8,3 +8,6 @@ python3 manage.py shell
 from django.core.management.utils import get_random_secret_key
 
 print(get_random_secret_key()) 
+
+
+
--- mysite/settings/production.py
+++ mysite/settings/production.py
@@ -1,11 +1,11 @@
 from .base import *
 
 
-ALLOWED_HOSTS = ["cuddly-bottle-production-tailwind-flowbite-auth.up.railway.app"]
+ALLOWED_HOSTS = ["cuddly-bottle-production-tailwindflowbiteauth.up.railway.app"]
 
 # FORM SUBMISSION
 # Comment out the following line and place your railway URL, and your production URL in the array.
-CSRF_TRUSTED_ORIGINS = ["https://cuddly-bottle-production-tailwind-flowbite-auth.up.railway.app"]
+CSRF_TRUSTED_ORIGINS = ["https://cuddly-bottle-production-tailwindflowbiteauth.up.railway.app"]
 
 SESSION_COOKIE_SECURE = True # handle security.W012
 CSRF_COOKIE_SECURE = True # handle security.W016
