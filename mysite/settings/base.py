508686852c09c06e529a6d6debc1e526fe7b925e
alex <croitoriu.alexandru.code@gmail.com>
2023-10-25 02:34:53 -0900

    buggsss

--- mysite/settings/base.py
+++ mysite/settings/base.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    "phonenumber_field",
+    # "phonenumber_field",
     'tailwind',
     'theme',
     'members',
